dc04b32ca89a0df6bc882f93d0a99a35887254ae
Ellen Bennett <ellenbennett@Ellens-MacBook-Air.local>
2023-08-14 21:30:47 -0600

    update

--- js/index.js
+++ js/index.js
@@ -3,30 +3,33 @@ let form = document.querySelector("form")
 function renderRepos (repoData) {
     let repoList = document.getElementById("repos-list")
     let repo = document.createElement("li")
-    repoList.appendChild(repo)
+    // repoList.innerHTML = " "
     repo.innerHTML = `
-    <p id="repo-name">Repo: 
-        <a href="${repoData.html_url}">${repoData.name}</a>
-    </p>
-    `
+        <p id="repo-name">Repo: 
+            <a href="${repoData.html_url}">${repoData.name}</a>
+        </p>
+        `
+    repoList.appendChild(repo) 
 }
 
 function createUser (userData) {
+    console.log(userData)
     let userLi = document.createElement("li")
         let userList = document.getElementById("user-list").appendChild(userLi)
         userLi.innerHTML = `
         <h4 id="username">Username:</h4>
         <p id="login">${userData.login}
-            <img class="avatar" src="${userData.avatar_url}" /img> 
+            <img class="avatar" src="${userData.avatar_url}"/img> 
+            <button type="button">Repos</button>
         </p>
         <a href="${userData.html_url}">Visit ${userData.login}'s page</a>
         `
+        
+        let userName = userData.login
+        let repoBtn = document.querySelector("button")
 
-        let userName = document.getElementById("login")
-
-        userName.addEventListener("click", (e) => {
-            console.log(userName.textContent)
-            fetch(`https://api.github.com/users/${userName.textContent}/repos`)
+        repoBtn.addEventListener("click", (e) => {
+            fetch(`https://api.github.com/users/${userName}/repos`)
             .then(resp => resp.json())
             .then(repoData => {
                 console.log(repoData)
@@ -38,6 +41,13 @@ function createUser (userData) {
 
 form.addEventListener("submit", (e) => {
     e.preventDefault()
+
+    let userList = document.getElementById("user-list")
+    userList.innerHTML = " "
+
+    let repoList = document.getElementById("repos-list")
+    repoList.innerHTML = " "
+
 
     let user = e.target.children[0].value
 
